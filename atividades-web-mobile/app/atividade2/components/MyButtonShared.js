189cdeb49d8d8ac10af47aeec692ec482037a376
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2026-04-04 13:27:51 +0000

    remove

--- atividades-web-mobile/app/atividade2/components/MyButtonShared.js
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export function MyButtonShared({ count, onClick }) {
-  return <button onClick={onClick}>I'm a shared button: {count}</button>;
-}
--- atividades-web-mobile/app/atividade2/components/MyPanelTitle.js
+++
@@ -1,3 +0,0 @@
-export default function MyPanelTitle({ count }) {
-  return <h3>Meu Painel com Botões Compartilhados: {count}</h3>;
-}
